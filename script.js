a4d810c688cef4bf86fc0d22d3a549424e3f67e5
vlevi96 <vlevi9609@gmail.com>
2021-10-20 14:42:03 +0200

    Szerdai óra 2.

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
 async function loadEvent() {
   const rootElement = document.getElementById("root");
   
-  const res = await fetch("https://restcountries.com/v3.1/name/peru");
-  const peru = await res.json();
-  console.log(peru);
+  const res = await fetch("https://restcountries.com/v3.1/all");
+  const countries = await res.json();
   
-  const peruO = peru[0];
+  //console.log(countries);
+  
+  for (const country of countries) {
+    //console.log(country.name.official);
+    const countryHTML = `
+          <section class="independent">
+          <img src="${country.flags.png}" alt="flag">
+          <h1>${country.name.official}</h1>
+          <h2>${country.capital}</h2>
+       
+          </section>
+      `;
+    rootElement.insertAdjacentHTML('beforeend', countryHTML);
+  }
+  
+
+  /*const peruO = peru[0];
   console.log(peruO);
 
-  const peruHTML = `
-        <section class="independent">
-        <img src="${peruO.flags.png}" alt="flag">
-        <h1>${peruO.name.official}</h1>
-        <h2>${peruO.capital[0]}</h2>
-        <ul>
-            <li>First language</li>
-        </ul>
-      
-        </section>
-    `;
   var peruLand = ``;
   if (peruO.landlocked == true) {
     var peruLand = `
@@ -29,8 +33,7 @@ async function loadEvent() {
   console.log(rootElement);
   //console.log(peruLand);
   
-  rootElement.insertAdjacentHTML('beforeend', peruHTML);
-  rootElement.insertAdjacentHTML('beforeend', peruLand);
+  rootElement.insertAdjacentHTML('beforeend', peruLand);*/
 }
 
 window.addEventListener("load", loadEvent);
